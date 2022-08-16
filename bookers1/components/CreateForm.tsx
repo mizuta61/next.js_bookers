e57f841fd5de972d9a0202118a2842da64978db2
yutomizuta <88771372+yutomizuta@users.noreply.github.com>
2022-08-16 16:03:59 +0900

    Revert "型指定"

--- bookers1/components/CreateForm.tsx
+++ bookers1/components/CreateForm.tsx
@@ -1,58 +1,37 @@
-import axios from "axios";
-import React, { useState } from "react";
-import Input from "@mui/material/Input";
-import Button from "@mui/material/Button";
-import MenuBookIcon from "@mui/icons-material/MenuBook";
+import axios from 'axios';
+import React, { useState } from 'react'
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 
-type Book = {
-  id: number;
-  title: string;
-  body: string;
-  created_at: string;
-  updated_at: string;
-};
+export default function CreateForm() {
+  const [book, setBook] = useState({title: "", body: ""});
 
-const CreateForm = () => {
-  const [book, setBook] = useState({ title: "", body: "" });
+  const handleChange = (input) => e => {
+    setBook({...book, [input] : e.target.value});
+  }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBook({ ...book, [e.target.name]: e.target.value });
-  };
-
-  const createBook = async () => {
-    await axios.post("http://localhost:3001/books", { book });
-  };
+  const createBook = async() => {
+    await axios.post("http://localhost:3001/books", {book})
+  }
 
   return (
     <>
-      <form onSubmit={createBook}>
-        <Input
-          type="text"
-          value={book.title}
-          placeholder="Title"
-          name="title"
-          onChange={handleChange}
-        />
-        <br></br>
-        <Input
-          type="text"
-          value={book.body}
-          placeholder="Body"
-          name="body"
-          onChange={handleChange}
-        />
-        <br></br>
-        <Button
-          type="submit"
-          variant="contained"
-          color="secondary"
-          size="small"
-          startIcon={<MenuBookIcon />}
-        >
-          Create
-        </Button>
-      </form>
+    <form onSubmit={createBook}>
+      <Input type="text" value={book.title} placeholder="Title" onChange={handleChange('title')} />
+      <br></br>
+      <Input type="text" value={book.body} placeholder="Body" onChange={handleChange('body')} />
+      <br></br>
+      <Button
+            type="submit"
+            variant="contained"
+            color="secondary"
+            size ="small"
+            startIcon={<MenuBookIcon />}
+      >
+        Create
+      </Button>
+    </form>
     </>
-  );
+  )
 }
-export default CreateForm;

b1d23d4b2a58bc3566a446b4e9bb57ed29fab306
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-08-18 01:44:59 +0900

    CrateForm.tsxの修正

--- front/src/components/CreateForm.tsx
+++ front/src/components/CreateForm.tsx
@@ -5,9 +5,13 @@ import Button from "@mui/material/Button";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import { Book } from "../types/Book";
+
+type BookCreate = Pick<Book, "title" | "body">
 
 const CreateForm = () => {
-  const [book, setBook] = useState({ title: "", body: "" });
+  const initialBook: BookCreate = { title: "", body: "" }
+  const [book, setBook] = useState(initialBook);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBook({ ...book, [e.target.name]: e.target.value });
@@ -18,49 +22,46 @@ const CreateForm = () => {
   };
 
   return (
-    <>
-      <Grid container>
-        <Grid
-          item
-          xs
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Box component="form" sx={{ width: "30%" }} onSubmit={createBook}>
-            <TextField
-              sx={{ marginBottom: "5px", width: "100%" }}
-              type="text"
-              value={book.title}
-              name="title"
-              onChange={handleChange}
-              variant="standard"
-              label="Title"
-            />
-            <br />
-            <TextField
-              sx={{ marginBottom: "5px", width: "100%" }}
-              type="text"
-              value={book.body}
-              name="body"
-              onChange={handleChange}
-              variant="standard"
-              label="Body"
-            />
-            <br />
-            <Button
-              sx={{ marginBottom: "15px", width: "100%" }}
-              type="submit"
-              variant="contained"
-              color="secondary"
-              startIcon={<MenuBookIcon />}
-            >
-              Create
-            </Button>
-          </Box>
-        </Grid>
+    <Grid container>
+      <Grid
+        item
+        xs
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box component="form" sx={{ width: "30%" }} onSubmit={createBook}	>
+          <TextField
+            sx={{ marginBottom: "5px", width: "100%" }}
+            type="text"
+            value={book.title}
+            name="title"
+            onChange={handleChange}
+            variant="standard"
+            label="Title"
+          />
+          <TextField
+            sx={{ marginBottom: "5px", width: "100%" }}
+            type="text"
+            value={book.body}
+            name="body"
+            onChange={handleChange}
+            variant="standard"
+            label="Body"
+          />
+          <Button
+            sx={{ marginBottom: "15px", width: "100%" }}
+            type="submit"
+            variant="contained"
+            color="secondary"
+            startIcon={<MenuBookIcon />}
+            formMethod="POST"
+          >
+            Create
+          </Button>
+        </Box>
       </Grid>
-    </>
+    </Grid>
   );
 };
 export default CreateForm;
